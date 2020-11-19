c9776dbb0d2e02e9df02e439bc5acdf119b3c421
Ayadi Tahar <ayaditahero2016@gmail.com>
2020-11-19 18:27:57 +0100

    added project Alert

--- src/App.js
+++ src/App.js
@@ -1,99 +1,103 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
-
+import React, { Component } from "react";
+import swal from "sweetalert";
+import "./App.css";
+import Person from "./Person/Person";
 class App extends Component {
+  componentDidMount() {
+    swal("This is a learning project made with ❤ by Clayton");
+  }
+
   state = {
     persons: [
-      { id: 'dlm', name: 'Max', age: 28 },
-      { id: 'pes', name: "Clayton", age:30 }
+      { id: "dlm", name: "Max", age: 28 },
+      { id: "pes", name: "Clayton", age: 30 },
     ],
-    showPersons: false
-  }
+    showPersons: false,
+  };
 
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
-  }
+    this.setState({ persons: persons });
+  };
 
   nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
+    const personIndex = this.state.persons.findIndex((p) => {
       return p.id === id;
-    })
+    });
 
     const person = {
-      ...this.state.persons[personIndex]
+      ...this.state.persons[personIndex],
     };
 
     person.name = event.target.value;
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons: persons} );
-  }
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
-
     const style = {
-      backgroundColor: 'green',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black',
-      }
-    }
+      backgroundColor: "green",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
+      },
+    };
 
     let persons = null;
 
-    if(this.state.showPersons) {
+    if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
-            click={() => this.deletePersonHandler(index)}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)}/>
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
           })}
         </div>
       );
       style.backgroundColor = "red";
-      style[':hover'] = {
-        backgroundColor: 'salmon'
-      }
+      style[":hover"] = {
+        backgroundColor: "salmon",
+      };
     }
 
-    let classes = [] 
+    let classes = [];
 
-    if (this.state.persons.length <=2){
-      classes.push('red');
+    if (this.state.persons.length <= 2) {
+      classes.push("red");
     }
-    if (this.state.persons.length <=1){
-      classes.push('bold');
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
     }
 
-    return(
-    <div className="App">
-      <h1 className={classes.join(' ')}>Hello I'm Clayton</h1>
-      <button
-        style={style}
-        onClick={this.togglePersonsHandler}>Switch Name
-      </button>
-      {persons}
-    </div>
+    return (
+      <div className="App">
+        <h1 className={classes.join(" ")}>Hello I'm Clayton</h1>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Switch Name
+        </button>
+        {persons}
+      </div>
     );
-
   }
 }
 
-export default (App);
+export default App;
